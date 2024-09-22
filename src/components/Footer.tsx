365c54cf15df27a25ac30021d746435f40b0ca46
josemagoo <joseenrique030812@gmail.com>
2024-09-22 00:45:05 -0400

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,19 +2,28 @@ import React, { useEffect, useState } from "react";
 import NextLink from "next/link";
 import { useRouter } from "next/router";
 import { getTranslation, Locale } from '../config/translation';
+import axios from "../config/axios";
 
 export default function Footer() {
   const router = useRouter();
-  const { locales, asPath } = router;
-
-  // Obtener el idioma actual del local storage o del router, o usar 'es' como valor por defecto
   const [currentLocale, setCurrentLocale] = useState<Locale>('es');
+  const [footerData, setFooterData] = useState<{ [key: number]: any[] }>({});
 
   useEffect(() => {
-    // Solo intentar obtener el idioma del localStorage en el cliente
     const savedLocale = localStorage.getItem('locale') as Locale;
     setCurrentLocale(savedLocale || router.locale || 'es');
-  }, [router.locale]); // Dependencia solo en router.locale
+
+    const fetchFooterData = async () => {
+      try {
+        const response = await axios.get(`/footer?lang=${savedLocale}`);
+        setFooterData(response.data);
+      } catch (error) {
+        console.error('Error al cargar los datos del footer:', error);
+      }
+    };
+
+    fetchFooterData();
+  }, [router.locale]);
 
   return (
     <footer className="footer bg-[#061A40] relative z-[200]">
@@ -27,59 +36,35 @@ export default function Footer() {
           <p className="text-white p-2 block font-bold">
             {getTranslation('contenido_interes', currentLocale)}
           </p>
-          <NextLink href="/catalogo" passHref>
-            <span className="text-white p-2 block opacity-70">
-              {getTranslation('catalogo', currentLocale)}
-            </span>
-          </NextLink>
-          <NextLink href="/trabajos" passHref>
-            <span className="text-white p-2 block opacity-70">
-              {getTranslation('trabajos', currentLocale)}
-            </span>
-          </NextLink>
-          <NextLink href="/empresa" passHref>
-            <span className="text-white p-2 block opacity-70">
-              {getTranslation('empresa', currentLocale)}
-            </span>
-          </NextLink>
-          <NextLink href="/politica" passHref>
-            <span className="text-white p-2 block opacity-70">
-              {getTranslation('politica_privacidad', currentLocale)}
-            </span>
-          </NextLink>
-          <NextLink href="/cookies" passHref>
-            <span className="text-white p-2 block opacity-70">
-              {getTranslation('politica_cookies', currentLocale)}
-            </span>
-          </NextLink>
-          <NextLink href="/aviso" passHref>
-            <span className="text-white p-2 block opacity-70">
-              {getTranslation('aviso_legal', currentLocale)}
-            </span>
-          </NextLink>
+          {footerData[1]?.map(item => (
+            <NextLink key={item.id} href={item.link} passHref>
+              <span className="text-white p-2 block opacity-70">
+                {item.title}
+              </span>
+            </NextLink>
+          ))}
         </div>
 
         <div className="p-5">
           <p className="text-white p-2 block font-bold">
             {getTranslation('necesitas_ayuda', currentLocale)}
           </p>
-          <a href="#" className="text-white p-2 block opacity-70">
-            {getTranslation('contactanos', currentLocale)}
-          </a>
-          <a href="#" className="text-white p-2 block opacity-70">WhatsApp</a>
-          <a href="#" className="text-white p-2 block opacity-70">Email</a>
-          <a href="#" className="text-white p-2 block opacity-70">Telegram</a>
-          <a href="#" className="text-white p-2 block opacity-70">Signal</a>
+          {footerData[2]?.map(item => (
+            <a key={item.id} href={item.link} className="text-white p-2 block opacity-70">
+              {item.title}
+            </a>
+          ))}
         </div>
 
         <div className="p-5">
           <p className="text-white p-2 block font-bold">
             {getTranslation('seguinos_en', currentLocale)}
           </p>
-          <a href="#" className="text-white p-2 block opacity-70">Instagram</a>
-          <a href="#" className="text-white p-2 block opacity-70">Facebook</a>
-          <a href="#" className="text-white p-2 block opacity-70">Twitter</a>
-          <a href="#" className="text-white p-2 block opacity-70">TikTok</a>
+          {footerData[3]?.map(item => (
+            <a key={item.id} href={item.link} className="text-white p-2 block opacity-70">
+              {item.title}
+            </a>
+          ))}
         </div>
       </div>
 
