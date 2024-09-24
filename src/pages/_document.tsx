1a63ac23ad58013848cbf3987248f705f473631c
josemagoo <joseenrique030812@gmail.com>
2024-09-23 20:18:24 -0400

    google tag manager

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,13 +11,17 @@ export default function Document() {
       w[l] = w[l] || [];
       w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
       const f = d.getElementsByTagName(s)[0];
-      const j = d.createElement(s) as HTMLScriptElement;  // Especificamos el tipo correcto
+      const j = d.createElement(s) as HTMLScriptElement;
       const dl = l != 'dataLayer' ? '&l=' + l : '';
       j.async = true;
       j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-      f.parentNode.insertBefore(j, f);
+      
+      if (f && f.parentNode) { // Verifica que f y f.parentNode no sean null
+        f.parentNode.insertBefore(j, f);
+      }
     })(window, document, 'script', 'dataLayer', 'GTM-P8M96BKF');
   }, []);
+
 
 
   return (
