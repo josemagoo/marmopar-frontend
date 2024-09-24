f4907d231d0ae620bbf2ff363707335523384f9e
josemagoo <joseenrique030812@gmail.com>
2024-09-23 21:19:39 -0400

    meta tags dynamic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -211,16 +211,15 @@ export default function Home() {
           href={`${process.env.NEXT_PUBLIC_URL_STORAGE}/${config?.favicon}`}
           type="image/x-icon"
         />
-        <meta name="description" content="Marmopar ofrece productos de alta calidad en mármol, granito y cuarzo en Paraguay. Ideal para proyectos residenciales y comerciales." />
-        <meta name="keywords" content="Mármol Paraguay, Granito Paraguay, Cuarzo Paraguay, Marmolería, Diseño de interiores, Mesadas de mármol, Mesadas de granito, Revestimientos en piedra natural" />
-        <meta name="author" content="Marmopar" />
-        <meta property="og:title" content="Marmopar - Mármol, Granito y Cuarzo en Paraguay" />
-        <meta property="og:description" content="Marmopar, especialistas en mármol, granito y cuarzo, brinda materiales premium para cocinas, baños y proyectos decorativos en Paraguay." />
+        <meta name="description" content={config?.rrss_description} />
+        <meta name="keywords" content={config?.keywords} />
+        <meta name="author" content={config?.title_site} />
+        <meta property="og:title" content={config?.rrss_title} />
+        <meta property="og:description" content={config?.rrss_description} />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.marmolpy.com" />
-        <meta property="og:image" content="https://admin.marmolpy.com/img/logo.png" />
-        <meta property="og:site_name" content="Marmopar" />
-        <meta property="og:locale" content="es_PY" />
+        <meta property="og:url" content={config?.rrss_url} />
+        <meta property="og:image" content={`${process.env.NEXT_PUBLIC_URL_STORAGE}/${config?.rrss_image}`} />
+        <meta property="og:site_name" content={config?.rrss_title} />
 
       </Head>
   
